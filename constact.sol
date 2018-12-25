d9ecb6280dab004cb035cddec6954650b627b9f8
ltstriker <ltuse163@163.com>
2018-12-25 13:35:14 +0800

    可以上传下载500byte
    因为定长为这么多。。。
    然后，下一步增加一些，尽量让他能够稍微的大一些。不过受到gaslimit限制，估计是最大也就是1kb多一点。。。所以下一步就改成1kb了。。。。

---
+++ constact.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.4.21;
+
+contract Node{
+    byte[500] public image;
+    Node next;
+    
+    function Node(byte[] res){
+        setImage(res);
+    }
+    
+    function getNext()public returns(Node){
+        return next;
+    }
+    
+    function setNext(Node _next)public{
+        next = _next;
+    }
+    
+    function getImage() public constant returns(byte[500] res){
+        return image;
+    }
+    
+    function setImage(byte[] res) public{
+        for(uint c1=0;c1<res.length;c1++){
+            image[c1]=res[c1];
+        }
+    }
+    
+    function attachToHead(address addr)public returns(bool){
+        bytes4 methodId = bytes4(keccak256("attach()"));
+        return addr.call(methodId);
+    }
+}
+
+contract manager{
+    // address master;
+    
+    Node public head;
+    
+    function manager(byte[] img) public{
+        head = new Node(img);
+        // master = sender;
+    }
+    
+    function getImage(uint index)public constant returns(byte[500] res){
+        Node temp = head;
+        for(uint c1=0;c1<index;c1++)
+            temp = temp.getNext();
+        return temp.getImage();
+    }
+    
+    function attach()public {
+        Node temp = Node(msg.sender);
+        temp.setNext(head);
+        head = temp;
+    }
+}
+
+
+
+
+
+
